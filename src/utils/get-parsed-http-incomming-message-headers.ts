a9dda434f6a626614e72b8119b7e318605c5a949
Daniel Madrid <dani_my98@Daniels-Mac-mini.local>
2025-11-11 02:05:30 -0500

    clean http headers schema.

--- src/utils/get-parsed-http-incomming-message-headers.ts
+++ src/utils/get-parsed-http-incomming-message-headers.ts
@@ -1,11 +1,7 @@
 import http from "node:http";
 import * as v from "valibot";
 
-const httpHeadersSchema = v.object({
-  "x-vix-user-id": v.string(),
-  "x-vix-country-code": v.string(),
-  "x-vix-platform": v.string(),
-});
+const httpHeadersSchema = v.object({});
 
 export default function getParsedHttpHeaders(
   incommingHttpHeaders: http.IncomingHttpHeaders,
